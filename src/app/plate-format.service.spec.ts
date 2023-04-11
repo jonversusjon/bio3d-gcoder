20181f4152ed0356a5a262030251ac8f92b3ed2a
jonversusjon <jtwhite200@gmail.com>
2023-04-10 23:09:53 -0700

    working on styling the print-position-picker

---
+++ src/app/plate-format.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PlateFormatService } from './plate-format.service';
+
+describe('PlateFormatService', () => {
+  let service: PlateFormatService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PlateFormatService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
